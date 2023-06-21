7b18a762d49527da6a7d5a53a6a91d88f63311f5
Kuba <j.barszcz02@gmail.com>
2023-06-21 15:29:03 +0200

    Zadania

---
+++ 1. Liczby pierwsze/1. Liczby pierwsze/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj ile liczb pierwszych wypisać:");
+        int n = int.Parse(Console.ReadLine());
+
+        int count = 0;
+        int number = 2;
+
+        while (count < n)
+        {
+            if (CzyPierwsza(number))
+            {
+                Console.WriteLine(number);
+                count++;
+            }
+
+            number++;
+        }
+
+        Console.ReadLine();
+    }
+
+    static bool CzyPierwsza(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ 3. Trójkąty/3. Trójkąty/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj wysokość trójkąta:");
+        int height = int.Parse(Console.ReadLine());
+
+        narysuj(height);
+
+        Console.ReadLine();
+    }
+
+    static void narysuj(int height)
+    {
+        int width = height * 2 - 1;
+
+        for (int row = 0; row < height; row++)
+        {
+            int starsCount = row * 2 + 1;
+            int spacesCount = (width - starsCount) / 2;
+
+            for (int i = 0; i < spacesCount; i++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int i = 0; i < starsCount; i++)
+            {
+                Console.Write("+");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
